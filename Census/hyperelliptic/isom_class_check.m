e790a31d7250cf1ea6282b21652b89f72021eac7
Jun Bo Lau <laujunbo@gmail.com>
2024-01-29 15:17:00 -0500

    updated hyperelliptic

--- Census/hyperelliptic/isom_class_check.m
+++ Census/hyperelliptic/isom_class_check.m
@@ -1,4 +1,4 @@
-OutputFileName := "new_isom_" cat InputFileName;
+OutputFileName := "isom_" cat InputFileName;
 LinesOfInputFile := Split(Read(InputFileName), "\n");
 
 L := #LinesOfInputFile;
@@ -26,16 +26,40 @@ function FindIndex(TxtFile, InitialPointCounts,StartingIndex)
     end for;
 end function;
 
+function AutomorphismGroupCorrected(F)
+    try
+        return AutomorphismGroup(F);
+    catch e
+        /* When this error occurs, Automoprhisms(F) is still assumed to return a list
+           of automorphisms which generates the full groups, but may be incomplete and/or
+           include repetitions. */
+        L := Automorphisms(F);
+        G := FreeGroup(#L);
+        rels := [];
+        for i in [1..#L] do
+            for j in [1..#L] do
+                g := Composition(L[i], L[j]);
+                for k in [1..#L] do
+                    if Equality(g, L[k]) then
+                        Append(~rels, G.i*G.j*G.k^(-1));
+                    end if;
+                end for;
+            end for;
+        end for;
+        return quo<G|rels>;
+    end try;
+end function;
+
 // Main loop: check for pairwise isomorphism by varying over elements of the same point counts
 i := 1;
 while i le L do
     lst := eval(LinesOfInputFile[i]);
-    ct := lst[1];
     supp := lst[2];
     F0 := FFConstruction(supp);
-
-    autsize := #AutomorphismGroup(Curve(A,supp));
-    fprintf OutputFileName, "[" cat "%o" cat "," cat "%o" cat "]" cat "\n", supp,autsize;
+    ct := [NumberOfPlacesOfDegreeOneECF(F0,n) : n in [1..6]];
+    G := IdentifyGroup(AutomorphismGroupCorrected(F0));
+    
+    fprintf OutputFileName, "[" cat "%o" cat "," cat "'" cat "%o" cat "'" cat "," cat "%o" cat "]" cat "\n", ct, G, supp[1];
 
     tmp := [F0];
     j := FindIndex(LinesOfInputFile,ct,i);
@@ -44,10 +68,12 @@ while i le L do
         lst2 := eval(LinesOfInputFile[ind]);
         supp2 := lst2[2];
         F02 := FFConstruction(supp2);
+        
         if forall(u){m : m in tmp | #Isomorphisms(F02,m) eq 0 } eq true then
             Append(~tmp,F02);
-            autsize := #AutomorphismGroup(Curve(A,supp2));
-            fprintf OutputFileName, "[" cat "%o" cat "," cat "%o" cat "]" cat "\n", supp2,autsize;
+            ct2 := [NumberOfPlacesOfDegreeOneECF(F0,n) : n in [1..6]];
+            G2 := IdentifyGroup(AutomorphismGroupCorrected(F02));
+            fprintf OutputFileName, "[" cat "%o" cat "," cat "'" cat "%o" cat "'" cat "," cat "%o" cat "]" cat "\n", ct2, G2, supp2[1];
         end if;
     end for;
     i := j + 1;
